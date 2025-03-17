70dfb9d38be1cd86cca9a23a4d8d7adf284d2a78
nbutluo <nbutluo@63.com>
2025-03-17 21:39:24 +0800

    first commit

---
+++ JaveSe/src/Demo01.java
@@ -0,0 +1,8 @@
+public class Demo01 {
+
+    public static void main(String[] args) {
+        String s = "哈哈哈";
+        // String表示要订一个一个字符串，String不是关键字，是一个类
+    }
+}
+~
---
+++ JaveSe/src/Demo2.java
@@ -0,0 +1,45 @@
+public class Demo2 { 
+    public static void main(String[] args) {
+        /*
+         *
+         * 数据类型
+         *
+         *   基本类型（8中）
+         *         整型 byte（1） short(2） int（4） long(8)
+         *         浮点  float(4) double(8)
+         *         字符 占2个字节
+         *          布尔 占1 字节
+         *
+         *   引用类型 ： 类、接口、数组
+         */
+
+
+        // 整型
+        int num1 = 10;
+        short num2 = 20;
+        byte num3 = 30;
+        long num4 = 30L;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+
+        // 浮点数
+        float f1 = 3.14F;
+        double f2 = 5.1458632;
+        System.out.println(f1);
+        System.out.println(f2);
+
+        // 字符
+        char c = '中';
+        System.out.println(c);
+
+        // 布尔
+        boolean b = false;
+        System.out.println(b);
+
+
+
+    }
+}
