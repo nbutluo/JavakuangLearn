73e1457f628c89ef10820f2f600d8e9230db4d0a
nbutluo <nbutluo@63.com>
2025-04-01 23:34:02 +0800

    变量与常量

---
+++ JaveSe/src/Demo07.java
@@ -0,0 +1,17 @@
+public class Demo07 {
+
+    // 实例变量
+    String userName;
+    int userAge;
+    static int salary = 2500;  // 类变量
+
+    public static void main(String[] args) {
+        // int i = 10;  // 局部变量
+
+        Demo07 demo07 = new Demo07();
+        System.out.println(demo07.userName);
+        System.out.println(demo07.userAge);
+
+        System.out.println(salary);
+    }
+}
---
+++ JaveSe/src/Demo08.java
@@ -0,0 +1,11 @@
+public class Demo08 {
+
+    // 常量 // 一旦初始化后不再变化的，不可改变，使用final关键字
+    // 修饰符不区分前后树勋
+
+    static final double PI = 3.1415296;
+
+    public static void main(String[] args) {
+        System.out.println(PI);
+    }
+}
