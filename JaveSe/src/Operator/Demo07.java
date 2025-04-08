6795f220734647df736207ee404168adf4b064e5
nbutluo <nbutluo@63.com>
2025-04-08 22:52:20 +0800

    字符串连接符

---
+++ JaveSe/src/Operator/Demo07.java
@@ -0,0 +1,11 @@
+package Operator;
+
+public class Demo07 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a += b);
+    }
+}
---
+++ JaveSe/src/Operator/Demo08.java
@@ -0,0 +1,20 @@
+package Operator;
+
+public class Demo08 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        // 字符串连接符
+        System.out.println("" + a + b);  //1020
+
+        System.out.println(a + b + ""); // 30
+
+        String s1 = "hello";
+        String s2 = 1 + "";
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(333);
+    }
+}
