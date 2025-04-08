f72f4888534eae66762a2c1a9808d41a34160d48
nbutluo <nbutluo@63.com>
2025-04-08 21:39:26 +0800

    运算符与包机制

---
+++ JaveSe/src/Operator/Demo04.java
@@ -0,0 +1,21 @@
+package Operator;
+
+public class Demo04 {
+    public static void main(String[] args) {
+        // 自增运算符
+
+        int a = 3;
+        int b = a++;  // b =3  a=4
+        int c = ++a;  // a=5 c=5
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        // 幂运算
+        double d = Math.pow(4,3);
+        System.out.println(d);
+    }
+
+
+}
---
+++ JaveSe/src/Operator/Demo06.java
@@ -0,0 +1,21 @@
+package Operator;
+
+public class Demo06 {
+    // 位运算符
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b = " + (a && b));
+
+        // 短路运算
+        int c = 5;
+        boolean d = (c < 4) && (c++ < 4);
+
+        System.out.println(d);
+        System.out.println(c);
+
+    }
+
+
+}
