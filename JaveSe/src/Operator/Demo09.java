eecab1e017cad6fe434764585af9b61604f52cc5
nbutluo <nbutluo@63.com>
2025-04-08 23:18:34 +0800

    三元运算符

---
+++ JaveSe/src/Operator/Demo09.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class Demo09 {
+    public static void main(String[] args) {
+
+        // 三元运算符
+        // x ? y : z
+
+        int score = 60;
+        String type = score < 60 ? "不及格" : "及格";
+
+        System.out.println(type);
+    }
+}
