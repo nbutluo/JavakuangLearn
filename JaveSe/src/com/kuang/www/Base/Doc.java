64d09b76f3224bea937985b61403c7af2c82c676
nbutluo <nbutluo@63.com>
2025-04-09 14:27:27 +0800

    Javadoc

---
+++ JaveSe/src/com/kuang/www/Base/Doc.java
@@ -0,0 +1,25 @@
+package com.kuang.www.Base;
+
+/**
+ * @author Luo.xiaogang
+ * @version 1.0
+ */
+public class Doc {
+
+    String name;
+
+    /**
+     *
+     * @since 1.8
+     * @param name
+     * @return
+     * @throws Exception
+     */
+    public String  test(String name) throws Exception {
+        return name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
